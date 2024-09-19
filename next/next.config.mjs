9ae8b40dc4cdc36791c7100258aa6f3c22414d92
siwolee <siwolee@student.42seoul.kr>
2024-09-19 21:53:41 +0900

    fix: infra: nginx 없애고 next 자체 서버 서빙

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export", // out dir에 static file export
-  distDir: "build",
-  images: {
-    unoptimized: true, // static option / optimize 불가
-  },
+  output: "standalone",
 };
+
 
 export default nextConfig;
