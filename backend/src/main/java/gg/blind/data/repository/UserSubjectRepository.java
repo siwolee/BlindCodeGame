736c609df20f33f57a1b325c6b767474a2639cf5
seungsje <seungsje@student.42seoul.kr>
2024-09-21 15:47:10 +0900

    [Feature] 전제 조회 Solved 수정

--- backend/src/main/java/gg/blind/data/repository/UserSubjectRepository.java
+++ backend/src/main/java/gg/blind/data/repository/UserSubjectRepository.java
@@ -5,11 +5,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import gg.blind.data.entity.User;
 import gg.blind.data.entity.UserSubject;
 
 public interface UserSubjectRepository extends JpaRepository<UserSubject, Long> {
-	List<UserSubject> findByUserId(Long userId);
+	List<UserSubject> findByUserIdAndIsSolvedTrue(Long userId);
 
 	Optional<UserSubject> findByUserIdAndSubjectIdAndIsSolvedTrue(Long userId, Long subjectId);
 }
